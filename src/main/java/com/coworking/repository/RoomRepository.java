d8bb4389cdcf4b468301483cf31bf8e8310f822c
Meybelin <perdomomeybel@gmail.com>
2025-11-24 19:57:39 -0600

    feat(room): agrega soporte para imágenes, precio, ubicación y búsqueda avanzada de disponibilidad
    - Nuevo metodo createRoom(dto, image)
    - manejo de features, price y otros
    - Logica de disponibilidad
    - nuevas queries en RoomRepository y ReservationRepository

--- src/main/java/com/coworking/repository/RoomRepository.java
+++ src/main/java/com/coworking/repository/RoomRepository.java
@@ -2,6 +2,13 @@ package com.coworking.repository;
 
 import com.coworking.model.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.time.LocalTime;
+import java.util.List;
+
+@Repository
 public interface RoomRepository extends JpaRepository<Room, Long> {
+    List<Room> findByCapacityGreaterThanEqual(Integer people);
+
 }
