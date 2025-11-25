b72a030afed182b63016dd97f6bf72fb807d1ae6
Meybelin <perdomomeybel@gmail.com>
2025-11-24 20:23:21 -0600

    refactor(config): eliminar llamada innecesaria a  toString() en WebConfig

--- src/main/java/com/coworking/config/WebConfig.java
+++ src/main/java/com/coworking/config/WebConfig.java
@@ -16,7 +16,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         Path uploadPath = Paths.get(uploadDir).toAbsolutePath().normalize();
-        String uploadAbsolutePath = "file:" + uploadPath.toString() + "/"; // file:/c:/.../
+        String uploadAbsolutePath = "file:" + uploadPath + "/"; // file:/c:/.../
 
         registry
                 .addResourceHandler("/uploads/**")
