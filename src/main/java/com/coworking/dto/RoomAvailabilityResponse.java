d8f9f00bb2d15767996ec7672fff5729f3f925b7
Meybelin <perdomomeybel@gmail.com>
2025-11-24 19:59:26 -0600

    feat(room): agrega RoomAvailabilityResponse DTO para el availability endpoint

---
+++ src/main/java/com/coworking/dto/RoomAvailabilityResponse.java
@@ -0,0 +1,14 @@
+package com.coworking.dto;
+
+import lombok.Data;
+
+@Data
+public class RoomAvailabilityResponse {
+    private Long id;
+    private String name;
+    private Integer capacity;
+    private String location;
+    private String imageUrl;
+    private boolean available;
+
+}
