04d22f710e5b0bce829112a11c1642a585d15ec3
Meybelin <perdomomeybel@gmail.com>
2025-12-16 14:08:51 -0600

    refactor(security): Eliminado de constructor manual usando @AllArgsconstructor

--- src/main/java/com/coworking/service/CustomUserDetailsService.java
+++ src/main/java/com/coworking/service/CustomUserDetailsService.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
