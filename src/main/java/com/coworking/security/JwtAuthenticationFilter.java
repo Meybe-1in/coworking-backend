77e8258b722d0d875930b3d301ffcf4b9c8a4a61
Meybelin <perdomomeybel@gmail.com>
2025-12-16 14:17:11 -0600

    - Early return si no existe Authorization Header - Cambio a email como subject estándar del token

--- src/main/java/com/coworking/security/JwtAuthenticationFilter.java
+++ src/main/java/com/coworking/security/JwtAuthenticationFilter.java
@@ -27,11 +27,20 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String authHeader = request.getHeader("Authorization");
-        if (authHeader != null && authHeader.startsWith("Bearer ")){
-            String jwt = authHeader.substring(7);
-            String username = jwtUtil.extractUsername(jwt);
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+
+        final String authHeader = request.getHeader("Authorization");
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = authHeader.substring(7);
+        String email = jwtUtil.extractUsername(token); //subject = email
 
         if (email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
 
