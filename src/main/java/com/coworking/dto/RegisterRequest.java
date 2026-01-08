9a7ee624e7d2bceafc16f4f2cf63830592611b57
Meybelin <perdomomeybel@gmail.com>
2026-01-07 21:21:34 -0600

    feat(auth): agregar campo termsAccepted en registro

--- src/main/java/com/coworking/dto/RegisterRequest.java
+++ src/main/java/com/coworking/dto/RegisterRequest.java
@@ -6,5 +6,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 public record RegisterRequest(
         String username,
         String email,
-        String password
+        String password,
+        Boolean termsAccepted
 ) {}
