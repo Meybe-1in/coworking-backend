197cd176a5dbd5beaf79c991365cd649a245acb3
Meybelin <perdomomeybel@gmail.com>
2026-02-02 09:06:36 -0600

    feat(auth): agregar entidad y repositorio PasswordResetToken

---
+++ src/main/java/com/coworking/model/PasswordResetToken.java
@@ -0,0 +1,48 @@
+package com.coworking.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "password_reset_tokens")
+public class PasswordResetToken {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @JoinColumn(name = "user_id", nullable = false)
+    private LocalDateTime expiryDate;
+
+    //metodos
+    public static PasswordResetToken create(User user) {
+        return new PasswordResetToken(
+                null,
+                UUID.randomUUID().toString(),
+                user,
+                LocalDateTime.now().plusHours(1) // token valido de 1 hora
+        );
+    }
+
+    public boolean isExpired(){
+        return expiryDate.isBefore(LocalDateTime.now());
+    }
+
+}
