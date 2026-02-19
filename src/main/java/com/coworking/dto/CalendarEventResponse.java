0aed35930fb00a26ae5d0f24c31cdc6bde1e63fa
Meybelin <perdomomeybel@gmail.com>
2026-02-19 15:22:56 -0600

    feat(calenddar): agregar DTO para reservacion de eventos, compatible con fullCalendar

---
+++ src/main/java/com/coworking/dto/CalendarEventResponse.java
@@ -0,0 +1,18 @@
+package com.coworking.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CalendarEventResponse {
+
+    private String title;
+    private LocalDateTime start;
+    private LocalDateTime end;
+}
