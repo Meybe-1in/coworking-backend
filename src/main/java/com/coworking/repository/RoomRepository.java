a5d71737987714e14c3e945866f31eb458ebce12
Meybelin <perdomomeybel@gmail.com>
2026-03-06 13:27:21 -0600

    feat(repository): Cambio en RoomRepository para ordenar por capacidad

--- src/main/java/com/coworking/repository/RoomRepository.java
+++ src/main/java/com/coworking/repository/RoomRepository.java
@@ -7,6 +7,5 @@ import java.util.List;
 
 @Repository
 public interface RoomRepository extends JpaRepository<Room, Long> {
-    List<Room> findByCapacityGreaterThanEqual(Integer people);
-    List<Room> findByCapacityGreaterThanEqualOrderByCapacityAsc(Integer people);
+    List<Room> findByCapacityOrderByCapacityAsc(Integer capacity);
 }
