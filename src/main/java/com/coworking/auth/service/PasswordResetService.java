171cac32829d91e3e0e201ec50920ec9370c8a03
Meybelin <perdomomeybel@gmail.com>
2026-04-08 15:26:20 -0600

    refactor(auth): integracion de servicio EmailSender con infraestructura base de emails

--- src/main/java/com/coworking/auth/service/PasswordResetService.java
+++ src/main/java/com/coworking/auth/service/PasswordResetService.java
@@ -1,8 +1,9 @@
 package com.coworking.auth.service;
 
 import com.coworking.auth.dto.ResetPasswordRequest;
-import com.coworking.email.service.EmailService;
+import com.coworking.domain.notification.EmailSender;
 import com.coworking.auth.model.PasswordResetToken;
+import com.coworking.email.template.EmailTemplate;
 import com.coworking.exception.BadRequestException;
 import com.coworking.user.model.User;
 import com.coworking.auth.repository.PasswordResetTokenRepository;
@@ -11,6 +12,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+
 @Service
 @RequiredArgsConstructor
 public class PasswordResetService {
@@ -18,7 +21,7 @@ public class PasswordResetService {
     private final UserRepository userRepository;
     private final PasswordResetTokenRepository tokenRepository;
     private final PasswordEncoder passwordEncoder;
-    private final EmailService emailService;
+    private final EmailSender emailSender;
 
     // resend link
     public void sendResetLink(String email) throws BadRequestException {
@@ -29,15 +32,19 @@ public class PasswordResetService {
             PasswordResetToken token = PasswordResetToken.create(user);
             tokenRepository.save(token);
 
-            emailService.sendPasswordResetEmail(
+            String link = "http://localhost:5173/reset-password?token=" + token.getToken();
+
+            emailSender.send(
                     user.getEmail(),
-                    token.getToken()
+                    "Recuperar contraseña",
+                    EmailTemplate.RESET_PASSWORD.getPath(),
+                    Map.of("link", link)
             );
         });
     }
 
     public void resetPassword(ResetPasswordRequest request) throws BadRequestException {
-        if (!request.getNewPassword().equals(request.getConfirmPassword())){
+        if (!request.getNewPassword().equals(request.getConfirmPassword())) {
             throw new BadRequestException("Las contraseñas no coinciden");
         }
 
@@ -46,7 +53,7 @@ public class PasswordResetService {
                 .orElseThrow(() ->
                         new BadRequestException("Token inválido"));
 
-        if (token.isExpired()){
+        if (token.isExpired()) {
             throw new BadRequestException("El token ha expirado");
         }
 
