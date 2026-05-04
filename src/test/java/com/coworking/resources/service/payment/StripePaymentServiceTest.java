e58169813e2e48b209c4b029a39a7b529d6e831e
Meybelin <perdomomeybel@gmail.com>
2026-05-04 10:12:44 -0600

    feat(test payment): agregar pruebas para StripePaymentService en escenarios exitosos y de error - se agrega test shouldCreatePaymentIntent (flujo exitoso) - se agrega test shouldThrowExceptionWhenReservationNotFound - se agrega test shouldThrowExceptionWhenStripeFails - se verifica la interacción con StripeClient usando Mockito.verify - se asegura que no se llame a Stripe cuando la reserva no existe

--- src/test/java/com/coworking/resources/service/payment/StripePaymentServiceTest.java
+++ src/test/java/com/coworking/resources/service/payment/StripePaymentServiceTest.java
@@ -1,5 +1,6 @@
 package com.coworking.resources.service.payment;
 
+import com.coworking.exception.NotFoundException;
 import com.coworking.payment.service.PaymentService;
 import com.coworking.payment.service.StripePaymentService;
 import com.coworking.payment.service.stripe.StripeClient;
@@ -47,5 +48,49 @@ class StripePaymentServiceTest {
         String clientSecret = paymentService.createPaymentIntent(1L);
 
         assertNotNull(clientSecret);
+
+        //verification
+        Mockito.verify(stripeClient, Mockito.times(1))
+                .createPaymentIntent(BigDecimal.valueOf(10), 1L);
+    }
+
+    @Test
+    void shouldThrowExceptionWhenReservationNotFound() {
+
+        // Arrange
+        Mockito.when(reservationRepository.findById(1L))
+                .thenReturn(Optional.empty());
+
+        // Act & Assert
+        assertThrows(NotFoundException.class, () -> {
+            paymentService.createPaymentIntent(1L);
+        });
+
+        // stripe nunca debe llamarse
+        Mockito.verifyNoInteractions(stripeClient);
+    }
+
+    @Test
+    void shouldThrowExceptionWhenStripeFails() {
+
+        // Arrange
+        Reservation reservation = new Reservation();
+        reservation.setId(1L);
+        reservation.setPrice(BigDecimal.valueOf(10));
+
+        Mockito.when(reservationRepository.findById(1L))
+                .thenReturn(Optional.of(reservation));
+
+        Mockito.when(stripeClient.createPaymentIntent(Mockito.any(), Mockito.any()))
+                .thenThrow(new RuntimeException("Stripe error"));
+
+        // Act & Assert
+        assertThrows(RuntimeException.class, () -> {
+            paymentService.createPaymentIntent(1L);
+        });
+
+        // verifica si intentó llamar a Stripe
+        Mockito.verify(stripeClient)
+                .createPaymentIntent(BigDecimal.valueOf(10), 1L);
     }
 }
