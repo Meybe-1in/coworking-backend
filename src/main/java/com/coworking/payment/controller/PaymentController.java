f6ad96d6c672e1b58d9851bb15c735ad8c416f20
Meybelin <perdomomeybel@gmail.com>
2026-05-05 21:24:21 -0600

    feat(pagos): asegurar endpoint de PaymentIntent con usuario autenticado

--- src/main/java/com/coworking/payment/controller/PaymentController.java
+++ src/main/java/com/coworking/payment/controller/PaymentController.java
@@ -2,10 +2,14 @@ package com.coworking.payment.controller;
 
 import com.coworking.payment.service.PaymentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/payments")
@@ -14,7 +18,13 @@ public class PaymentController {
     private final PaymentService paymentService;
 
     @PostMapping("/intent/{reservationId}")
-    public String createPaymentIntent(@PathVariable Long reservationId){
-        return paymentService.createPaymentIntent(reservationId);
+    public ResponseEntity<Map<String, String>> createPaymentIntent(@PathVariable Long reservationId, Authentication authentication){
+        String email = authentication.getName();
+
+        String clientSecret= paymentService.createPaymentIntent(reservationId, email);
+
+        return ResponseEntity.ok(Map.of(
+                "ClientSecret", clientSecret
+        ));
     }
 }
