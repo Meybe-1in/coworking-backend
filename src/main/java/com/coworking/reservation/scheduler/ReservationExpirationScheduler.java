f5bd51db3f4d5ad13931e01d94556a64c6df5bb2
Meybelin <perdomomeybel@gmail.com>
2026-05-18 10:17:28 -0600

    feat: expirar reservaciones pendientes automáticamente

---
+++ src/main/java/com/coworking/reservation/scheduler/ReservationExpirationScheduler.java
@@ -0,0 +1,43 @@
+package com.coworking.reservation.scheduler;
+
+import com.coworking.reservation.enums.ReservationStatus;
+import com.coworking.reservation.model.Reservation;
+import com.coworking.reservation.repository.ReservationRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class ReservationExpirationScheduler {
+    private final ReservationRepository reservationRepository;
+
+    @Scheduled(fixedRate = 60000) //un minuto
+    @Transactional
+    public void expiredPendingReservations(){
+
+        Instant limit = Instant.now().minus(Duration.ofMinutes(15));
+
+        List<Reservation> expiredReservations =
+                reservationRepository
+                        .findByStatusAndCreatedBefore(
+                                ReservationStatus.PENDING,
+                                limit
+                        );
+
+        for (Reservation reservation : expiredReservations){
+            reservation.setStatus(ReservationStatus.EXPIRED);
+            log.info(
+                    "Reservación {} expirada automáticamente",
+                    reservation.getId()
+            );
+        }
+    }
+}
