b55c14aa896e7dfcd69f1c08497ff9574d0d51fc
Meybelin <perdomomeybel@gmail.com>
2026-05-19 09:31:43 -0600

    feat: agregar codigo para usar en reservas expiradas

--- src/main/java/com/coworking/reservation/enums/ErrorCodeReservation.java
+++ src/main/java/com/coworking/reservation/enums/ErrorCodeReservation.java
@@ -3,5 +3,6 @@ package com.coworking.reservation.enums;
 public enum ErrorCodeReservation {
     UNAUTHORIZED_ACTION,
     RESERVATION_ALREADY_CANCELLED,
-    RESERVATION_ALREADY_STARTED
+    RESERVATION_ALREADY_STARTED,
+    RESERVATION_EXPIRED
 }
