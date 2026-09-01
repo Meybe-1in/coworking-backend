bcaf0ee221d44edb029aa8d4fe3e2fdcf0213cb8
Meybelin <perdomomeybel@gmail.com>
2026-08-31 18:56:49 -0600

    feat: preparar actualizacion de usuarios

---
+++ src/main/java/com/coworking/admin/dto/UpdateUserRequest.java
@@ -0,0 +1,22 @@
+package com.coworking.admin.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UpdateUserRequest {
+    @NotBlank(message = "El nombre de usuario es obligatorio")
+    private String username;
+
+    @NotBlank(message = "El correo es obligatorio")
+    @Email(message = "Formato de correo inválido")
+    private String email;
+
+    @NotBlank(message = "El rol es obligatorio")
+    private String role;
+}
