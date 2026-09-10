8ca89b691c92fb0829480d38638e2ef4ebdf7244
Meybelin <perdomomeybel@gmail.com>
2026-09-09 21:48:57 -0600

    feat(setting): agregar consulta de configuracion del sistema

--- src/main/java/com/coworking/admin/settings/repository/SystemSettingsRepository.java
+++ src/main/java/com/coworking/admin/settings/repository/SystemSettingsRepository.java
@@ -4,6 +4,10 @@ import com.coworking.admin.settings.entity.SystemSettings;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface SystemSettingsRepository extends JpaRepository<SystemSettings, Long> {
+
+    Optional<SystemSettings> findFirstByOrderByIdAsc();
 }
